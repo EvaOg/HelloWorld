5d310dc03e38c869d701d91467e2272c49f027c0
Evvaog <evvaog@gmail.com>
2024-05-04 17:01:32 +0200

    Added "Add dot" in StrManipulation

--- src/string/StrManipulation.java
+++ src/string/StrManipulation.java
@@ -30,6 +30,20 @@ public class StrManipulation {
         System.out.println(Arrays.toString(arrInt));
     }
 
+    private static void addDot(String str3){
+        String[] arr3 = str3.split(",\\s");
+
+        if (!(arr3[arr3.length - 1].equals("."))) {
+            String[] newArray = new String[arr3.length + 1];
+            for (int i = 0; i < arr3.length; i++) {
+                newArray[i] = arr3[i];
+                newArray[newArray.length - 1] = ".";
+            }
+            System.out.print(Arrays.toString(newArray));
+        }
+    }
+
+
     public static void main(String[] args) {
 
         //In the following exercises you need to clean the input by removing certain parts of the provided string.
@@ -49,6 +63,9 @@ public class StrManipulation {
         // "123, 45, -34.231, 0, -1, -12312, 12312, 23423". Replace all negative values with null (“0”).
         // A new, comma separated string should be created, e.g.
         // "123,45,0,231,0,0,0,12312,23423".  Also, all spaces should be removed, if any.
+
+        addDot(str3); // Add a dot at the end, if there is no
+
 
 
     }
