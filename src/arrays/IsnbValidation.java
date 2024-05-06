5f0c3d9448c5091b28dbbdb4e2ae0f9a81c3d7ca
Evvaog <evvaog@gmail.com>
2024-05-06 16:46:21 +0200

    final Version for IsbnValidation

--- src/arrays/IsnbValidation.java
+++ src/arrays/IsnbValidation.java
@@ -14,10 +14,8 @@ public class IsnbValidation {
 
         //public static int calculateChecksumISBN13(int[] digits) { ... }
         int[] isbn12 = new int[] {9, 7, 8, 6, 0, 0, 1, 1, 9, 1, 2, 5};
-        int[] isbn14 = new int[] {9, 7, 8, 6, 0, 0, 1, 1, 9, 1, 2, 5};
 
-        calculateChecksumISBN13(isbn12, isbn14);
-        System.out.println(number13);
+        calculateChecksumISBN13(isbn12);
 
 
         /////////////////////////////////Second Exercise/////////////////////////
@@ -30,9 +28,7 @@ public class IsnbValidation {
         }
 
         isbn13[12] = number13;
-
         System.out.println(Arrays.toString(isbn13));
-
 
 
         //convert array into string
@@ -45,45 +41,31 @@ public class IsnbValidation {
             isbnInt[i] = Character.getNumericValue(splitArray.charAt(i));
         }
 
-        System.out.println(Arrays.toString(isbnInt));
-
         System.out.println(isValidISBN13(isbnInt));
 
     }
 
     private static boolean isValidISBN13(int[] isbnInt) {
-        if (isbnInt[12] != calculateAllNumber(isbnInt)) {
+        if (isbnInt[12] != calculateChecksumISBN13(isbnInt)) {
              return false;
         }
          return true;
     }
 
-    static public int calculateAllNumber(int [] isbnInt){
-        int sum = 0;
-        for (int i = 0; i < isbnInt.length - 1 ; i++) {
-            sum += isbnInt[i];
-            System.out.print(sum + " ");
-        }
-
-        number13 = sum % 9;
-        System.out.println("number13 ist " + number13);
-        return number13;
-    }
 
 
-    private static int calculateChecksumISBN13(int[] isbn12, int[] isbn14) {
+    private static int calculateChecksumISBN13(int[] isbn12) {
         int sum = 0;
 
+        //create temporary array to store new values
+        int[] copyisbn12  = new int[isbn12.length];
         for (int i = 1; i < isbn12.length; i += 2) {
-            isbn14[i] = isbn12[i] * 3;
+            copyisbn12[i] = isbn12[i] * 3;
         }
 
-        System.out.println(isbn14);
-        for (int i = 0; i < isbn14.length; i ++) {
-            sum += isbn14[i];
+        for (int i = 0; i < copyisbn12.length; i ++) {
+            sum += copyisbn12[i];
         }
-        System.out.println(Arrays.toString(isbn14));
-        System.out.println(sum);
 
         return number13 = sum % 9;
     }
